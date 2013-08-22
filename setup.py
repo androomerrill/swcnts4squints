ca7ad1ef0cd5e97d512bb0a4b4412b23513d78ed
Andrew Merrill <androomerrill@gmail.com>
2013-08-21 22:48:10 -0400

    updating setup.py to enable setuptools installations

--- setup.py
+++ setup.py
@@ -4,6 +4,32 @@ major, minor1, minor2, s, tmp = sys.version_info
 #if major==2 and minor1 < 7 or major < 2:
 #    raise SystemExit("""cntapp requires Python 2.6 or later""")
 
+from setuptools import setup, find_packages
+
+setup(name='swcnts4squints',
+      version=0.5,
+      description="python scripts",
+      long_description="""\
+              Simple app for calculating physical properties of carbon nanotubes""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='python scripts',
+      author='Andrew Merril',
+      author_email='androomerrill@gmail.com',
+      url='https://github.com/androomerrill/swcnts4squints',
+      license='BSD 2-Clause',
+      packages=find_packages(exclude=['docs', 'ez_setup', 'examples',
+                                      'tests', 'symlinks']),
+      include_package_data=True,
+      exclude_package_data = {'': ['README.md']},
+      zip_safe=False,
+      install_requires=['numpy', 'scipy'],
+      entry_points={
+          'gui_scripts': [
+              'swcnts4squints = swcnts4squints.swcnts4squints:main',
+              ]
+          }
+      )
+
 if sys.platform == 'darwin':
     from setuptools import setup
 
