1f27dcfd28e2bf543c2400e5d7f3198d939f315b
Andrew Merrill <androomerrill@gmail.com>
2013-12-13 14:33:01 -0500

    removing subpackages now available through sknano package

--- swcnts4squints/nanogen/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import division, print_function, absolute_import
-
-__docformat__ = 'restructuredtext'
-
-from .nanotube import *
-
-__all__ = [s for s in dir() if not s.startswith('_')]
